c5106d2f96cef29b3e56ee1f22757a9bbfc34cea
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-04-26 20:17:06 +0300

    append component `Input`

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Container } from './container';
 export { default as Header } from './header';
+export { default as Input } from './input';
 export { default as Logo } from './logo';
