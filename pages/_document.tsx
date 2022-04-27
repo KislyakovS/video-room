0d61e75541c8f755761382499daee4144edc6b7d
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-04-27 21:15:20 +0300

    added fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+    return (
+        <Html lang="en">
+            <Head>
+                <title>Video Room</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Raleway&display=swap" rel="stylesheet" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+export default Document;
