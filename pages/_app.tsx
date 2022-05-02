6808f8f65bd403f72d5ad2a49eb1111f8973dcea
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-05-02 12:45:55 +0300

    append utils style file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
+import '../styles/utils.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
     return <Component {...pageProps} />
