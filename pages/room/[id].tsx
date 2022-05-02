69c9ee5dc6d0508f46fa0dab9d0c28d9f342fd0b
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-05-02 06:09:30 +0300

    append css var `--border-radius`

--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 
-import { Header, Logo, Container } from 'components';
+import { Header, Logo, Container, FormSearch, FormSendMessage, Input } from 'components';
 
 const Room: NextPage = () => {
     const router = useRouter();
@@ -13,8 +13,22 @@ const Room: NextPage = () => {
                 <title>Room - {router.query.id}</title>
             </Head>
             <Container>
-                <Header leftSide={<Logo href='/' />} />
-                <main>
+                <Header
+                    leftSide={<Logo href='/' />}
+                    center={<FormSearch />}
+                />
+                <main className="room-main">
+                    <section>
+                        <div className="video"></div>
+                        <ul className="next-video">
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </section>
+                    <section className="chat">
+                        Chat
+                        <FormSendMessage />
+                    </section>
                 </main>
             </Container>
         </>
