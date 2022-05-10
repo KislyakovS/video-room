315700707e270a57d0517cfbd88625bec5440c09
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-05-10 21:39:20 +0300

    fix page `Not Found` remove image

--- ui/not-found/index.tsx
+++ ui/not-found/index.tsx
@@ -1,8 +1,5 @@
 import { FC } from 'react';
-import Image from 'next/image';
 import clsx from 'clsx';
-
-import notFound from './not-found.png';
 
 import styles from './not-found.module.css';
 
@@ -16,7 +13,6 @@ const NotFound: FC<NotFoundProps> = ({ className }) => {
             <span className={styles.code}>404</span>
             <h3 className={styles.title}>Not Found</h3>
             <span className={styles.legend}>Sorry, we were unable to find that page.</span>
-            <Image src={notFound} alt='Sorry, we were unable to find that page.' />
         </section>
     )
 };
