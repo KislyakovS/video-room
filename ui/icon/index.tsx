982e34b3ef47549777fee40f578dc95590490f0f
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-05-19 00:59:22 +0300

    remove `console.log`

--- ui/icon/index.tsx
+++ ui/icon/index.tsx
@@ -10,8 +10,6 @@ import styles from './icon.module.css';
 const Icon: FC<IconProps> = ({ className, type, size = Sizes.middle }) => {
     const I = ICONS[type];
 
-    console.log(I);
-
     const iconClasses = clsx(className, styles[size]);
 
     return <I className={iconClasses} />
