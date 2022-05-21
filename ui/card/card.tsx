3e76ae34add868fa2c67976c1f3e929194c53fc0
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-05-22 00:47:55 +0300

    fix image in component `Card`

--- ui/card/card.tsx
+++ ui/card/card.tsx
@@ -9,7 +9,7 @@ const Card: FC<CardProps> = ({ className, poster, title, duration, ...props }) =
     return (
         <article {...props} className={clsx(className, styles.wrapper)}>
             <div className={styles.poster}>
-                <img src={poster} alt={`Movie poster - ${title}`} />
+                <img src={poster} alt={`Movie poster - ${title}`} loading='lazy' decoding='async' />
                 <span className={styles.duration}>{duration}</span>
             </div>
             <h3 className={styles.title} title={title}>{title}</h3>
